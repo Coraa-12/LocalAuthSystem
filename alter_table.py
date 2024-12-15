3b25c7e0a01ac0bcca86ab1da86e000a63a5f578
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2024-12-15 19:41:59 +0700

    Testing 1

--- alter_table.py
+++ alter_table.py
@@ -1,7 +1,1 @@
-from app import db, app
-from sqlalchemy import text
-
-# Add the reset_token and profile_picture columns to the user table
-with app.app_context():
-    with db.engine.connect() as connection:
-        connection.execute(text('ALTER TABLE user ADD COLUMN profile_picture VARCHAR(200)'))
+print("Test")
